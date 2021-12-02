12ed9b1601e006535288fb0a13b838c94391d95b
S545389 <89318303+S545389@users.noreply.github.com>
2021-12-01 20:08:07 -0600

    files to repo

--- models/costume.js
+++ models/costume.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose") 
 const costumeSchema = mongoose.Schema({ 
  costume_type: String, 
- size: String, 
- cost: Number 
+ size: {
+    type: String,
+    maxlength: 19,
+    minlength: 2
+},  
+ cost: {
+    type: Number,
+    min: 20,
+   max: 50
+} 
 }) 
  
 module.exports = mongoose.model("Costume", costumeSchema) 
